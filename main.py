8508d4d989c1ac9001ec3623d2dce777a785a030
Kyle Shores <kyle.shores44@gmail.com>
2026-06-01 19:30:05 -0500

    adding packaging structure

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mandelbrot!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ mandelbrot/main.py
@@ -0,0 +1,8 @@
+from mandelbrot.mandelbrot import runit
+
+def main():
+    runit()
+
+
+if __name__ == "__main__":
+    main()
